c8f0f8ae35128a424be7e68aec24731896fba551
Lohithayenugu <47069512+Lohithayenugu@users.noreply.github.com>
2019-07-15 00:00:37 -0500

    ICP3

--- ICP7/Task3.py
+++ ICP7/Task3.py
@@ -8,7 +8,9 @@ from nltk import wordpunct_tokenize,pos_tag,ne_chunk
 response=rq.get("https://en.wikipedia.org/wiki/Google")
 cont=BeautifulSoup(response.content,"html.parser")
 #print(cont.text)
+
 text = 'Google was founded in 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a privately held company on September 4, 1998. An initial public offering (IPO) took place on August 19, 2004, and Google moved to its headquarters in Mountain View, California, nicknamed the Googleplex. In August 2015, Google announced plans to reorganize its various interests as a conglomerate called Alphabet Inc. Google is Alphabet\'s leading subsidiary and will continue to be the umbrella company for Alphabet\'s Internet interests. Sundar Pichai was appointed CEO of Google, replacing Larry Page who became the CEO of Alphabet. ';
+text = 'Former President Barack Obama will speak to young people at the University of Chicago on Monday. Returning to the city for that will be his first public event since leaving the White House.'
 #Tokenization
 print("_________________________________Tokenization_______________________________________")
 stokens = nltk.sent_tokenize(text)
@@ -38,9 +40,9 @@ for w in wtokens:
 print("_________________________________Trigrams_______________________________________")
 new_trigrams = []
 c = 0
-while c < len(wtokens) - 2:
-    new_trigrams.append((wtokens[c], wtokens[c+1], wtokens[c+2]))
-    c += 2
+while c < len(wtokens) - 1:
+    new_trigrams.append((wtokens[c], wtokens[c+1]))
+    c += 1
 for trigram in new_trigrams:
     print(trigram)
 
---
+++ ICP7/ed/__init__.py
@@ -0,0 +1,4 @@
+print('adff'.replace('df','12'))
+str = 'qwerty'
+str.replace('qw','fg')
+print(str)
